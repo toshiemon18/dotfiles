715b5d4c24004b840d596c065d2dba786cd62288
Toshiaki Seino <st12318@gmail.com>
2018-01-11 17:29:47 +0900

    installスクリプトを追加

---
+++ install.sh
@@ -0,0 +1,13 @@
+# NeoVim
+ln -s $PWD/nvim/init.vim $XDG_CONFIG_HOME/nvim/init.vim
+
+# Vim8
+ln -s $PWD/vim/vimrc $HOME/.vimrc
+ln -s $PWD/vim/gvimrc $HOME/.gvimrc
+
+# Zsh
+ln -s $PWD/zsh/.zshrc $HOME/.zshrc
+ln -s $PWD/zsh/.zshrc.pre-oh-my-zsh $HOME/.zsh_simple_prompt
+
+# tmux
+ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf

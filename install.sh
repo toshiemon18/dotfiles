776a48d2ac6a8d0e9dea09c524cd0be2ae7cb45d
Toshiaki Seino <st12318@gmail.com>
2018-01-12 15:36:22 +0900

    Fix target and source

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 # NeoVim
-ln -s $PWD/nvim/init.vim $XDG_CONFIG_HOME/nvim/init.vim
+# mkdir $XDG_CONFIG_HOME/nvim
+ln -fis $PWD/nvim $XDG_CONFIG_HOME/
 
 # Vim8
-ln -s $PWD/vim/vimrc $HOME/.vimrc
-ln -s $PWD/vim/gvimrc $HOME/.gvimrc
+ln -fis $PWD/vim/vimrc $HOME/.vimrc
+ln -fis $PWD/vim/gvimrc $HOME/.gvimrc
 
 # Zsh
-ln -s $PWD/zsh/.zshrc $HOME/.zshrc
-ln -s $PWD/zsh/.zshrc.pre-oh-my-zsh $HOME/.zsh_simple_prompt
+ln -fis $PWD/zsh/.zshrc $HOME/.zshrc
+ln -fis $PWD/zsh/.zsh_simple_prompt $HOME/.zsh_simple_prompt
+ln -fis $PWD/zsh/.switch_proxy.zsh $HOME/.switch_proxy.zsh
 
 # tmux
-ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
+ln -fis $PWD/tmux/tmux.conf $HOME/.tmux.conf

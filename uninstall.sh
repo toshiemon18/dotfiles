1d8dfb84faa4373fed395ba9ddd507f9a6030127
Toshiaki Seino <st12318@gmail.com>
2018-01-12 15:36:40 +0900

    Add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+# NeoVim
+rm -rf $XDG_CONFIG_HOME/nvim/nvim
+
+# Vim8
+rm $HOME/.vimrc
+rm $HOME/.gvimrc
+
+# Zsh
+rm $HOME/.zshrc
+rm $HOME/.zsh_simple_prompt
+rm $HOME/.switch_proxy.zsh
+
+# tmux
+rm $HOME/.tmux.conf

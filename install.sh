73bdde9688a0046b8c27c89e29de17ef302668a3
Toshiaki Seino <st12318@gmail.com>
2024-02-24 08:06:31 +0900

    fix install script

--- install.sh
+++ install.sh
@@ -20,5 +20,5 @@ ln -fis $PWD/zsh/.switch_proxy.zsh $HOME/.switch_proxy.zsh
 # tmux
 ln -fis $PWD/tmux/tmux.conf $HOME/.tmux.conf
 
-# alacritty
-ln -fis $PWD/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
+# Sheldon
+ln -fis $PWD/sheldon $XDG_CONFIG_HOME

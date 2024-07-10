305c6d0b59a9ce6c58d43b6f3caf197f61dd69db
Toshiaki Seino <st12318@gmail.com>
2024-07-11 02:50:45 +0900

    remove built-in function scope

--- nvim/lua/configs/plugins/telescope.lua
+++ nvim/lua/configs/plugins/telescope.lua
@@ -9,7 +9,7 @@ local function keymaps()
   keymap.set("n", "<Leader>c", builtin.commands,             { desc = "Telescope: Lists vim autocommands" })
   keymap.set("n", "<Leader>o", builtin.find_files,           { desc = "Telescope: Open files" })
   keymap.set("n", "<Leader>d", builtin.diagnostics,          { desc = "Telescope: Lists diagnostics" })
-  keymap.set("n", "<Leader><Leader>", builtin.builtin,       { desc = "Telescope: Open builtin function" })
+  -- keymap.set("n", "<Leader><Leader>", builtin.builtin,       { desc = "Telescope: Open builtin function" })
   keymap.set("n", "<Leader>a", builtin.lsp_document_symbols, { desc = "Telescope: List LSP document symbols" })
 end
 

4aece04619cada62a018266780b013a5571eba53
Toshiaki <toshiaki.seino@gree.net>
2024-10-09 05:39:13 +0900

    improve obsidian toolkit

--- nvim/lua/configs/plugins/obsidian.lua
+++ nvim/lua/configs/plugins/obsidian.lua
@@ -6,12 +6,6 @@ local function daily_path()
   return vaultPath .. dailyDirName
 end
 
-local function todays_note_path()
-	local note_filename = os.date("/%Y/%m/%Y-%m-%d") .. ".md"
-
-	return  daily_path() .. note_filename
-end
-
 function open_today_note()
 	local note_path = todays_note_path()
 
@@ -36,10 +30,20 @@ vim.api.nvim_create_user_command(
   {}
 )
 
+vim.api.nvim_create_user_command(
+  "OpenJournal",
+  function()
+    local note_filename = os.date("/%Y/%m/%Y-%m-%d") .. ".md"
+
+    vim.cmd("e " .. daily_path() .. note_filename)
+  end,
+  {}
+)
+
 keymap.set(
 	"n",
 	"<leader><leader>ot",
-	":e " .. todays_note_path() .. "<CR>",
+  "<cmd>OpenJournal<CR>",
 	{
 		silent = true,
 		desc = "Open today's obsidian notebook",

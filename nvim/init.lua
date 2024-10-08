9f7fd2735163c96f5b3a0d7a557787b8adc536fa
Toshiaki Seino <st12318@gmail.com>
2024-10-08 23:51:40 +0900

    fix treesitter function

--- nvim/init.lua
+++ nvim/init.lua
@@ -55,3 +55,12 @@ require("lazy").setup(plugins, {
 vim.opt.filetype = "plugin", "indent", "on"
 vim.opt.syntax = "on"
 
+-- vim help を開くと、パーサプロバイダがないとtreesitterがエラーになる
+-- ref: https://zenn.dev/kawarimidoll/articles/18ee967072def7
+vim.treesitter.start = (function(wrapped)
+  return function(bufnr, lang)
+    lang = lang or vim.fn.getbufvar(bufnr or '', '&filetype')
+    pcall(wrapped, bufnr, lang)
+  end
+end)(vim.treesitter.start)
+

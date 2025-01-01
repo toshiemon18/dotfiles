a3935b0b2b5205688b93a1879e0c140abc142eb4
Toshiaki Seino <st12318@gmail.com>
2025-01-02 01:51:02 +0900

    update telescope

--- nvim/lua/configs/plugins/telescope.lua
+++ nvim/lua/configs/plugins/telescope.lua
@@ -1,39 +1,101 @@
 local function keymaps()
-	local keymap = vim.keymap
-	local builtin = require("telescope.builtin")
+  local keymap = vim.keymap
+  local builtin = require("telescope.builtin")
 
-	keymap.set("n", "<Leader>m", builtin.marks, { desc = "Telescope: Lists vim marks" })
-	keymap.set("n", "<Leader>k", builtin.keymaps, { desc = "Telescope: Lists normal mode keymappings" })
-	keymap.set("n", "<Leader>l", builtin.buffers, { desc = "Telescope: Lists open buffers in current neovim instance" })
-	keymap.set("n", "<Leader>0", builtin.oldfiles, { desc = "Telescope: Lists previously open files" })
-	keymap.set("n", "<Leader>c", builtin.commands, { desc = "Telescope: Lists vim autocommands" })
-	keymap.set("n", "<Leader>o", builtin.find_files, { desc = "Telescope: Open files" })
-	keymap.set("n", "<Leader>d", builtin.diagnostics, { desc = "Telescope: Lists diagnostics" })
-	-- keymap.set("n", "<Leader><Leader>", builtin.builtin,       { desc = "Telescope: Open builtin function" })
-	keymap.set("n", "<Leader>a", builtin.lsp_document_symbols, { desc = "Telescope: List LSP document symbols" })
+  -- ファイル検索
+  keymap.set("n", "<leader>tf", function()
+    builtin.find_files({
+      no_ignore = false,
+      hidden = true
+    })
+  end, { desc = "Telescope: List Files" })
+  -- live grep
+  keymap.set("n", "<leader>tr", function()
+    builtin.live_grep()
+  end, { desc = "Telescope: Live Grep" })
+  keymap.set("n", "<Leader>tb", builtin.buffers, { desc = "Telescope: Lists open buffers in current neovim instance" })
+
+  -- キーマップ検索
+  keymap.set("n", "<Leader>tk", builtin.keymaps, { desc = "Telescope: Lists normal mode keymappings" })
+
+  -- nvimのautocmdを検索する
+  keymap.set("n", "<Leader>ta", builtin.commands, { desc = "Telescope: Lists vim autocommands" })
+
+  -- LSP の diagnostic 一覧を検索する
+  keymap.set("n", "<Leader>d", builtin.diagnostics, { desc = "Telescope: Lists diagnostics" })
+
+  -- File browser
+  keymap.set("n", "<leader>ts", function()
+    require("telescope").extensions.file_browser.file_browser({
+      path = "%:p:h",
+      cwd = vim.fn.expand("%:p:h"),
+      respect_gitignore = false,
+      hidden = true,
+      grouped = true,
+      previewer = false,
+      initial_mode = "normal",
+      layout_config = { height = 40 }
+    })
+  end)
 end
 
 local function setup()
-	local actions = require("telescope.actions")
+  local actions = require("telescope.actions")
+  local filebrowser_actions = require("telescope").extensions.file_browser.actions
 
-	require("telescope").setup({
-		defaults = {
-			mappings = {
-				n = { ["<ECS>"] = actions.close }
-			},
-			layout_strategy = "vertical",
-		}
-	})
+  require("telescope").setup({
+    defaults = {
+      mappings = {
+        n = {
+          -- Telescope を閉じる
+          ["<ECS>"] = actions.close,
+          ["q"] = actions.close
+        }
+      },
+      layout_strategy = "vertical",
+      layout_config = {
+        prompt_position = "top"
+      },
+      sorting_strategy = "ascending"
+    },
+    extensions = {
+      file_browser = {
+        theme = "dropdown",
+        highjack_netrw = true,
+        mapping = {
+          ["i"] = {
+            ["<C-w>"] = function()
+              vim.cmd("normal vbd")
+            end
+          },
+          ["n"] = {
+            ["N"] = filebrowser_actions.create,
+            ["h"] = filebrowser_actions.goto_parent_dir,
+            ["/"] = function()
+              vim.cmd("startinsert")
+            end
+          }
+        }
+      }
+    }
+  })
 end
 
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-fzf-native.nvim"
-	},
-	config = function()
-		setup()
-		keymaps()
-	end,
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make"
+    },
+    "nvim-telescope/telescope-file-browser.nvim"
+  },
+  config = function()
+    local _, telescope = pcall(require, "telescope")
+    telescope.load_extension("file_browser")
+
+    setup()
+    keymaps()
+  end,
 }

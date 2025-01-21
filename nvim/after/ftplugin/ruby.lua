9e1a81f9e7eeddb338e1e84426bb1513973c58e5
Toshiaki <toshiaki.seino@gree.net>
2025-01-21 11:09:25 +0900

    disable ruby config

--- nvim/after/ftplugin/ruby.lua
+++ nvim/after/ftplugin/ruby.lua
@@ -1,8 +1,7 @@
-local func = require "func"
 -- Ruby
 
 -- Rubyのbool値を返すメソッド名を判定できるようにする
-vim.opt.iskeyword:append("?")
+-- vim.opt.iskeyword:append("?")
 
 -- Indent
 vim.bo.cindent = false
@@ -13,13 +12,13 @@ vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 
-vim.api.nvim_create_augroup("RSpec", {})
-vim.api.nvim_create_autocmd({"BufWinEnter", "BufNewFile"}, {
-  patern = { "*_spec.rb" },
-  command = "set filetype=ruby.rspec"
-})
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = { "Rakefile", "Schemafile" },
-  callback = "set filetype=ruby"
-})
+-- vim.api.nvim_create_augroup("RSpec", {})
+-- vim.api.nvim_create_autocmd({"BufWinEnter", "BufNewFile"}, {
+--   patern = { "*_spec.rb" },
+--   command = "set filetype=ruby.rspec"
+-- })
+-- vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+--   pattern = { "Rakefile", "Schemafile" },
+--   callback = "set filetype=ruby"
+-- })
 

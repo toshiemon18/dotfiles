806aeb55772f2170347d90108d60162b5d249750
Toshiaki <toshiaki.seino@gree.net>
2025-01-21 11:14:04 +0900

    add render-markdown configs

--- nvim/lua/configs/plugins/render_markdown.lua
+++ nvim/lua/configs/plugins/render_markdown.lua
@@ -3,7 +3,28 @@ return {
   name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require('render-markdown').setup({})
+    require('render-markdown').setup({
+      heading = {
+        width = "block",
+        icons = {},
+      },
+      -- ref: https://eiji.page/blog/neovim-render-markdown-nvim/
+      checkbox = {
+        checked = { scope_highlight = "@markup.strikethrough" },
+        custom = {
+          -- デフォルトの`[-]`であるtodoは削除
+          todo = { raw = "", rendered = "", highlight = "" },
+          canceled = {
+            raw = "[-]",
+            rendered = "󱘹",
+            scope_highlight = "@markup.strikethrough",
+          },
+        },
+      },
+      code = {
+        width = "block",
+      },
+    })
     vim.opt.conceallevel = 2
   end,
 }

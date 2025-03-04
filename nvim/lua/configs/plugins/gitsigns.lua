401f970bfb2bada72effc4a0776f4bb2d7c0fe1a
Toshiaki <toshiaki.seino@gree.net>
2025-03-04 18:27:29 +0900

    add blame keymap

--- nvim/lua/configs/plugins/gitsigns.lua
+++ nvim/lua/configs/plugins/gitsigns.lua
@@ -17,6 +17,7 @@ return {
     vim.keymap.set('n', '[g', ':Gitsigns prev_hunk<CR>', opts)
     vim.keymap.set('n', '<C-g><C-p>', ':Gitsigns preview_hunk<CR>', opts)
     vim.keymap.set('n', '<C-g><C-v>', ':Gitsigns blame_line<CR>', opts)
+    vim.keymap.set('n', '<C-g><C-b>', ':Gitsigns blame<CR>', opts)
     vim.keymap.set('n', '<C-g>a', ':Gitsigns stage_buffer<CR>', opts)
   end,
 }
